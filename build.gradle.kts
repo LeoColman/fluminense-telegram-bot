40b62564aa3ec04bcb55218e5c0a79a37863bf72
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-08-27 21:05:05 -0300

    Implement 2 other commands

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "br.com.colman"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/FluminenseTelegramBot.kt
+++ src/main/kotlin/FluminenseTelegramBot.kt
@@ -5,9 +5,10 @@ import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.TelegramFile
 
 
-val options = listOf(
+val torcidaOptions = listOf(
     listOf("Fluminense Eterno Amor", "É por isso que eu canto", "E visto esse manto", "Orgulho de ser Tricolor"),
 
     listOf("Desde que eu nasci, te acompanhei", "Fluminense eu sei, até a morte, contigo estarei", "Haja o que houver", "Onde quer que eu vá", "Comigo vou levar, as cores que herdei", "Verde branco e grená"),
@@ -29,9 +30,18 @@ fun main() {
         dispatch {
             command("nense") {
                 bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "NENSE! 🇮🇹🇮🇹🇮🇹")
-                options.random().forEach {
+                torcidaOptions.random().forEach {
                     bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = it)
                 }
+            }
+
+            command("fabio") {
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Fáaaaaaaaaaaaaabio!! 🇮🇹🥅🧱️⚽️🏃‍♂️🇮🇹")
+            }
+
+            command("vence") {
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Vitória Fluminense! 🇮🇹🇮🇹")
+                bot.sendPhoto(chatId = ChatId.fromId(message.chat.id), photo = TelegramFile.ByUrl("https://i.pinimg.com/originals/08/7f/1f/087f1f5b45e57cfc9793693976d07aeb.jpg"))
             }
         }
     }
